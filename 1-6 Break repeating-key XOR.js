a0199651e1bac578e68943022b5da55ee3b8a2f3
Frank de Groot - Schouten <fjtdg@hotmail.com>
2015-01-16 20:23:56 +0000

    1-6 keysize candidates.

--- 1-6 Break repeating-key XOR.js
+++ 1-6 Break repeating-key XOR.js
@@ -1,11 +1,11 @@
 var string1 = 'this is a test';
 var string2 = 'wokka wokka!!!';
 
-function hamming(string1, string2) {
+function hamming(byte1, byte2) {
   var distance = 0;
   for(var i = 0; i < string1.length; i++) {
-    var byte1 = string1.charCodeAt(i);
-    var byte2 = string2.charCodeAt(i);
+    //var byte1 = string1.charCodeAt(i);
+    //var byte2 = string2.charCodeAt(i);
     var diff = byte1 ^ byte2;
     for(var j = 0; j < 8; j++) {
       distance += diff & 1;
@@ -39,6 +39,18 @@ function getFile() {
 
 function crack(cryptbase64) {
   var bytes = new Buffer(cryptbase64, 'base64');
+  var diffs = [];
+  for (var keysize = 2; keysize < 40; keysize++) {
+    var firstBytes = bytes.slice(0, keysize);
+    var secondBytes = bytes.slice(keysize, 2 * keysize);
+    var diff = 0;
+    for (var i = 0; i < keysize; i++) {
+      diff += hamming(firstBytes.readUInt8(i), secondBytes.readUInt8(i));
+    }
+    diffs.push({keysize: keysize, diff: Math.ceil(diff / keysize)});
+  }
+  diffs.sort(function (a, b) { return a.diff - b.diff; });
+  console.log(diffs.slice(0, 3));
   return bytes;
 }
 
